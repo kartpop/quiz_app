27e26a74d641fb5b8f1c67d438074421f0e97e5b
KartikeyaPophali <kartikeyapophali@gmail.com>
2023-10-24 23:01:59 +0530

    feat: use CrossAxisAlignment attribute to keep text left aligned on results screen

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -13,22 +13,60 @@ class QuestionsSummary extends StatelessWidget {
         child: Column(
           children: summaryData.map(
             (data) {
-              return Row(
-                children: [
-                  Text(((data['question_index'] as int) + 1).toString()),
-                  Expanded(
-                    child: Column(
-                      children: [
-                        Text(data['question'] as String),
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        Text(data['user_answer'] as String),
-                        Text(data['correct_answer'] as String),
-                      ],
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 21.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      ((data['question_index'] as int) + 1).toString(),
+                      style: TextStyle(
+                        color: data['user_answer'] == data['correct_answer']
+                            ? Color.fromARGB(255, 129, 255, 244)
+                            : Color.fromARGB(255, 205, 105, 223),
+                        fontSize: 21,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  )
-                ],
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            data['question'] as String,
+                            style: const TextStyle(
+                              color: Color.fromARGB(213, 255, 255, 255),
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            data['user_answer'] as String,
+                            style: const TextStyle(
+                              color: Color.fromARGB(255, 205, 105, 223),
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            data['correct_answer'] as String,
+                            style: const TextStyle(
+                              color: Color.fromARGB(255, 129, 255, 244),
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               );
             },
           ).toList(),
